43d470ca68f92f613a6f5dee3a9443bf2ebfb675
K-Philleani <964202623@qq.com>
2021-02-11 22:07:57 +0800

    typescript组件

---
+++ src/components/popup/index.ts
@@ -0,0 +1,54 @@
+interface Ipopup {
+  width?: string
+  height?: string
+  title?: string
+  position?: string
+  mask?: boolean
+  content?: () => void
+}
+
+interface Icomponent {
+  tempContainer: HTMLElement
+  init: () => void
+  template: () => void
+  handle: () => void
+}
+
+
+class Popup implements Icomponent{
+  tempContainer
+  constructor(private settings: Ipopup) {
+    this.settings = Object.assign({
+      width: '100%',
+      height: '100%',
+      title: '',
+      position: 'center',
+      mask: true,
+      content: function() {}
+    }, this.settings)
+    this.init()
+  }
+  // 初始化
+  init() {
+    this.template()
+  }
+  // 创建模板
+  template() {
+    this.tempContainer = document.createElement('div')
+    this.tempContainer.innerHTML = `
+      <h1>Hello</h1>
+    `
+    document.body.appendChild(this.tempContainer)
+    console.log(111)
+  }
+  // 时间操作
+  handle() {
+
+  }
+}
+
+function popup(options: Ipopup) {
+  return new Popup(options)
+}
+
+export default popup
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
-import a from './a'
-import './icon.css'
-a()
+import './index.css'
+import popup from './components/popup/index'
+
+let listItem: NodeListOf<Element> = document.querySelectorAll('#list li')
+
+for (let i=0; i<listItem.length; i++) {
+  listItem[i].addEventListener('click', function() {
+    let url:string = this.dataset.url
+    let title:string = this.dataset.title
+    popup({})
+  })
+}
